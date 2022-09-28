100efa033c95de92aff5acbec07918b0996a3d65
William-silva-Developer <william958silva@gmail.com>
2022-09-27 23:07:35 -0300

    função com e sem retorno

---
+++ mergulho em javascript/Function.js
@@ -0,0 +1,13 @@
+//FUNÇÃO SEM RETORNO.
+function somar(a,b){
+    console.log('Soma: '+(a+b));
+
+};
+
+somar(2,6);
+
+//FUNÇÃO COM RETORNO.
+function soma(a,b){
+    return 'Soma de A + B é: '+(a,b);
+};
+console.log(soma(1,6));
