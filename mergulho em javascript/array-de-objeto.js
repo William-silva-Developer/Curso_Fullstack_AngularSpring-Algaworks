a4b24d3bfa13f31914e6d6729d2a43328a2a788e
William-silva-Developer <william958silva@gmail.com>
2022-09-27 21:21:56 -0300

    array de objetos

---
+++ mergulho em javascript/array-de-objeto.js
@@ -0,0 +1,22 @@
+
+var person1 = {
+    name: 'William',
+    age: 35
+};
+
+var person2 = {
+    name: 'Daniel',
+    age: 40
+};
+var person3 = {
+    name: 'Caio',
+    age: 35
+};
+
+var list = [person1,person2,person3];
+
+for (var i of list){
+    console.log(i);
+    
+};
+
