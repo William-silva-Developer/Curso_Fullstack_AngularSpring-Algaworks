85c1ed02b072629c94f096ded70a187408832f8f
William-silva-Developer <william958silva@gmail.com>
2022-09-27 21:01:29 -0300

    gerando o script hello e objeto

---
+++ mergulho em javascript/helloWorld.js
@@ -0,0 +1,3 @@
+var batata = 'World';
+
+console.log('Hello '+ batata)
---
+++ mergulho em javascript/objeto.js
@@ -0,0 +1,9 @@
+var person = {
+    name: 'William',
+    age: 35,
+    sex: 'male'
+};
+
+console.log('Nome: '+person.name);
+console.log('Idade: '+person.age);
+console.log('Sexo: '+person.sex);
