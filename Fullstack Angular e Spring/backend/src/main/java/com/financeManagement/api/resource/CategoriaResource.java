3425bc63a847b82006b7220ef6e3f5fa2e2486f8
William-silva-Developer <william958silva@gmail.com>
2022-09-29 23:22:12 -0300

    método salvar e buscar por id criado e adição do jackson no properties

--- Fullstack Angular e Spring/backend/src/main/java/com/financeManagement/api/resource/CategoriaResource.java
+++ Fullstack Angular e Spring/backend/src/main/java/com/financeManagement/api/resource/CategoriaResource.java
@@ -1,10 +1,20 @@
 package com.financeManagement.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Locale.Category;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.financeManagement.api.model.Categoria;
 import com.financeManagement.api.repository.CategoriaRepository;
@@ -18,9 +28,46 @@ public class CategoriaResource {
 	
 	private CategoriaRepository categoriaRepor;
 	
+	//MÉTODO PARA LISTAR TODAS AS CATEGORIAS
+	
 	@GetMapping
 	public List<Categoria> toList(){
 		return categoriaRepor.findAll();
 	};
-
+	
+	//MÉTODO PARA SALVAR UMA NOVA CATEGORIA
+	
+	@PostMapping
+	public ResponseEntity<Categoria> toInsert(@RequestBody Categoria categoria, HttpServletResponse response){
+		Categoria categoriaSalv = categoriaRepor.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentServletMapping().path("{/codigo}")
+				.buildAndExpand(categoriaSalv.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalv);
+	};
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> toSearch(@PathVariable Long codigo) {
+		
+		return categoriaRepor.findById(codigo)
+				.map(category -> ResponseEntity.ok().body(category))
+				.orElse(ResponseEntity.notFound().build());
+		
+		 
+		
+		  
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
