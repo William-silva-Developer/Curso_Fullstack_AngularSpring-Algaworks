54657b7b385922f6c43cde550858021e4f6ce53c
William-silva-Developer <william958silva@gmail.com>
2022-10-05 22:08:56 -0300

    usando event binding

--- meuProjeto/src/app/Components/first/first.component.ts
+++ meuProjeto/src/app/Components/first/first.component.ts
@@ -10,6 +10,18 @@ export class FirstComponent implements OnInit {
   name: string = "william";
   nome: string = "Angular.";
 
+  adicionar() {
+    console.log(`Adicionando ${this.nome}`);
+
+    const numero = Math.round(Math.random() * 100);
+    this.nome = this.name + numero;
+  }
+
+
+  alterarNome(event: any){
+    this.name = event.target.value;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
