495ae90d51f3f522297477cbda833d47b3d17205
William-silva-Developer <william958silva@gmail.com>
2022-10-05 23:06:27 -0300

    usando two-way data binding

--- meuProjeto/src/app/Components/second/second.component.ts
+++ meuProjeto/src/app/Components/second/second.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class SecondComponent implements OnInit {
 
 
-  name!: string;
+  
+  nome!: string;
 
-adicionar(name: string){
 
-  this.name =name;
-
-}
+adicionando(nome: string){
+  this.nome = nome;
+};
 
 
   constructor() { }
