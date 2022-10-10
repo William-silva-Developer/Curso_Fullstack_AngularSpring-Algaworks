065ec4e31c575fe3398d5aef09123a8acfe532d9
William-silva-Developer <william958silva@gmail.com>
2022-10-10 00:17:04 -0300

    melhoramento dos métodos na directive para mudar estado dos inputs

--- Fullstack Angular e Spring/frontend/src/app/Components/directive/directive.directive.ts
+++ Fullstack Angular e Spring/frontend/src/app/Components/directive/directive.directive.ts
@@ -1,10 +1,24 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appDirective]'
 })
 export class DirectiveDirective {
 
+  @HostBinding('style.backgroundColor')fundo!: string;
+
+//QUANDO O CURSOR FICAR NO INPUT GANHARÁ A COR #ede1e1
+  @HostListener('focus') cursorOn(){
+    this.fundo = '#ede1e1';
+  };
+
+   //QUANDO O CURSOR SAIR DO INPUT PEDERÁ A COR
+  @HostListener('blur') cursorOff(){
+    this.fundo = 'transparent';
+  }
+
+
+  /*
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2
@@ -21,5 +35,5 @@ export class DirectiveDirective {
   @HostListener('blur') cursorOff(){
     this.renderer.setStyle(this.elementRef.nativeElement,'background-color', 'transparent');
   }
-
+*/
 }
